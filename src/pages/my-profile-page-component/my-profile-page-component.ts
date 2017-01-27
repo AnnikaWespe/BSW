3653d31b176d54666888e536a376a65804d9d375
Annika Westphaeling <Annikawespe@gmail.com>
2017-01-27 09:51:33 +0100

    Feierabend Commit nachgeholt

--- src/pages/my-profile-page-component/my-profile-page-component.ts
+++ src/pages/my-profile-page-component/my-profile-page-component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, trigger, state, style, animate, transition} from '@angular/core';
+import {Component, Input, trigger, state, style, animate, transition, keyframes} from '@angular/core';
 import {NavController, NavParams} from 'ionic-angular';
 
 import {UserDetailProvider} from './user-detail/user-detail-provider';
@@ -10,28 +10,40 @@ import {UserDetail} from "./user-detail/UserDetail";
   providers: [UserDetailProvider],
   animations: [
     trigger('show', [
-      state('false', style({
-        'max-height': '0'
+      state('no', style({
+        'max-height': '0',
+        'transition-property': 'all',
+        'transition-duration': '.5s',
+        'transition-timing-function': 'cubic-bezier(0, 1, 0.5, 1)'
       })),
-      state('true', style({
+      state('yes', style({
         'height': '*',
-        '-webkit-transition': 'height .5s',
-        'transition': 'height .5s'
-      })),
-      transition('* => *', animate(1000))
+        'overflow-y': 'hidden',
+        'max-height': '500px', /* approximate max height */
+
+        'transition-property': 'all',
+        'transition-duration': '.5s',
+        'transition-timing-function': 'cubic-bezier(0, 1, 0.5, 1)'
+      }))
     ])
   ]
 })
 export class MyProfilePageComponent {
   title: string = "Mein Profil";
-  showContentOnPosition: Array<boolean> = [
-    true, false, false, false
+  showContentOnPosition: Array<string> = [
+    "yes", "no", "no", "no"
   ];
   userDetail: UserDetail;
   optionsSalutation = ["Frau", "Herr", "Firma"];
   submitted: boolean = false;
-  onSubmit(){this.submitted = true};
-  get diagnostic(){return JSON.stringify(this.userDetail)};
+
+  onSubmit() {
+    this.submitted = true
+  };
+
+  get diagnostic() {
+    return JSON.stringify(this.userDetail)
+  };
 
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public profileDataProvider: UserDetailProvider) {
@@ -39,15 +51,10 @@ export class MyProfilePageComponent {
   }
 
 
-
   toggleContent(i) {
-    //TODO: Check if let ... of someArray works in Typescript
-    //for (let boolean of this.showContentOnPosition){
-    //  boolean = false;
-    //};
-    this.showContentOnPosition = [false, false, false, false];
-    this.showContentOnPosition[i] = true;
+    for (let j = 0; j < this.showContentOnPosition.length; j++) {
+      this.showContentOnPosition[j] = "no";
+    };
+    this.showContentOnPosition[i] = "yes";
   }
-
-
 }
--- src/pages/partner-page-component/partner-page-component.ts
+++ src/pages/partner-page-component/partner-page-component.ts
@@ -1,14 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import {Geolocation} from 'ionic-native';
 
 
 @Component({
   selector: 'partner-page-component',
   templateUrl: 'partner-page-component.html'
 })
-export class PartnerPageComponent {
-
+export class PartnerPageComponent implements OnInit{
+  title: string = "Partner";
+  location:{};
   constructor(public navCtrl: NavController, public navParams: NavParams) {}
-
-
+  ngOnInit(){
+    Geolocation.getCurrentPosition().then((position) => {
+      this.location = position.coords;
+    })
+  }
 }
