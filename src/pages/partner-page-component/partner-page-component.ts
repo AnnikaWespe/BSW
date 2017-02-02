34d3d8c0123a76e6ed8c562efb6843cb8a818c19
Annika Westphaeling <Annikawespe@gmail.com>
2017-02-02 18:23:30 +0100

    nächster Schritt Partner nachladen

--- src/pages/partner-page-component/partner-page-component.ts
+++ src/pages/partner-page-component/partner-page-component.ts
@@ -10,12 +10,37 @@ import {PartnerService} from "./partner-service";
 })
 export class PartnerPageComponent implements OnInit {
   title: string = "Partner";
+  mode = "Observable";
+
+  showDropdown: boolean[] = [false, false];
+
   errorMessage: string;
   returnedObject: any;
   partnersJson: any;
-  partners: any;
-  mode = "Observable";
   location: {latitude: number, longitude: number};
+
+  partners: any;
+  offlinePartners: any;
+  onlinePartners: any;
+  travelPartners: any;
+  vehiclePartners: any;
+  displayedPartners: any;
+
+  numberOfOfflinePartners: number;
+  numberOfOnlinePartners: number;
+  numberOfTravelPartners: number;
+  numberOfVehiclePartners: number;
+  numberOfPartnersTotal: number;
+
+  numberOfPagesTotal: number;
+  numberOfPagesOffline: number;
+  numberOfPagesOnline: number;
+  numberOfPagesTravel: number;
+  numberOfPagesVehicle: number;
+
+
+
+
   constructor(public navCtrl: NavController, public navParams: NavParams, private partnerService: PartnerService) {
   }
 
@@ -23,50 +48,44 @@ export class PartnerPageComponent implements OnInit {
     this.getLocationData();
   };
 
-  /*x(){
+  getLocationData() {
     Geolocation.getCurrentPosition().then((position) => {
       this.location = position.coords;
       this.getPartners(this.location);
     }, (err) => {
       console.log(err);
     })
-  }*/
-
-
-  getLocationData() {
-    return new Promise((resolve, reject) => {
-      Geolocation.getCurrentPosition({timeout: 20000, enableHighAccuracy: false})
-        .then((position) => {
-          this.location = position.coords;
-          this.getPartners(this.location);
-          resolve();
-        })
-        .catch(
-          (err) => {
-            console.error('Could not read current location');
-            console.log(err.code + err.message);
-            reject(err);
-          });
-    })
   }
 
   getPartners(location) {
     this.partnerService.getPartners(location)
       .subscribe(
-        body => {this.returnedObject = body.json();
+        body => {
+          this.returnedObject = body.json();
           this.partnersJson = body;
           this.partners = this.returnedObject.contentEntities;
-          console.log("partners: " + this.partners);
-          console.log("error: " + this.errorMessage);
-          this.processData();
+          this.displayedPartners = this.partners;
+
+          this.onlinePartners = this.returnedObject.originalSearchResults.bucketToSearchResult.ONLINEPARTNER.contentEntities;
+          this.offlinePartners = this.returnedObject.originalSearchResults.bucketToSearchResult.OFFLINEPARTNER.contentEntities;
+          this.travelPartners = this.returnedObject.originalSearchResults.bucketToSearchResult.TRAVELOFFER.contentEntities;
+          this.vehiclePartners = this.returnedObject.originalSearchResults.bucketToSearchResult.VEHICLEOFFER.contentEntities;
         },
         error => this.errorMessage = <any>error);
 
   }
 
-  processData(){
-
+  toggleVisibility(position) {
+    let isVisible = this.showDropdown[position];
+    this.showDropdown = [false, false];
+    this.showDropdown[position] = !isVisible;
   }
+
+  display(partnerType){
+    this.displayedPartners = this[partnerType];
+    this.toggleVisibility(0);
+  }
+
 }
 
 
