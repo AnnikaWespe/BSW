4fa37dc5c54089e237b238a4b81b7ce0658401b9
Annika Westphaeling <Annikawespe@gmail.com>
2017-03-13 12:35:28 +0100

    Zwischenspeichern

--- src/pages/partner-page-component/partner-map/styled-map-partners-directive.ts
+++ src/pages/partner-page-component/partner-map/styled-map-partners-directive.ts
@@ -30,7 +30,7 @@ const svg = `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.
 </filter>
 </defs>
 <path id="Marker" class="cls-1" d="M4711.01,3827.65h96.31a14.779,14.779,0,0,1,14.82,14.75v73.77a14.788,14.788,0,0,1-14.82,14.76h-37.09l-11.07,35.04-11.84-35.04h-36.31a14.788,14.788,0,0,1-14.82-14.76V3842.4A14.779,14.779,0,0,1,4711.01,3827.65Z" transform="translate(-4693 -3827.66)"/>
-    <image x="10" y="-18" width="110" height="110" xlink:href=`
+    `
 
 
 
@@ -38,9 +38,24 @@ const svg = `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.
 @Directive({
   selector: 'styled-map-partners',
 })
-export class StyledMapPartnersDirective implements OnChanges {
+export class StyledMapPartnersDirective implements OnChanges, OnInit {
 
   @Input() partners: any[];
+
+  partnersWithoutDuplicatePositions(){
+    this.partners.map((partner, index) =>
+    {
+      let originLat = partner.location.latitude;
+      let originLon = partner.location.longitude;
+      let duplicates = [];
+      this.partners.forEach((partnerToCheck, index) => {
+        if (partnerToCheck.location.longitude === originLon && partnerToCheck.location.latitude){
+          duplicates.push(index);
+        };
+      });
+      if (duplicates.length >1){console.log(duplicates)};
+    })
+  }
 
   constructor(private googleMapsWrapper: GoogleMapsAPIWrapper) {
   }
@@ -51,6 +66,10 @@ export class StyledMapPartnersDirective implements OnChanges {
         this.setMapOptions(map);
         this.placeMarkers(map);
       });
+  }
+
+  ngOnInit(){
+    this.partnersWithoutDuplicatePositions();
   }
 
   private setMapOptions(map: any) {
@@ -66,7 +85,7 @@ export class StyledMapPartnersDirective implements OnChanges {
       fontSize = 18;
       x = "52"
     }
-    else if (stringLength < 11){
+    else if (stringLength < 12){
       fontSize = 18;
       x = "24"
     }
@@ -74,11 +93,30 @@ export class StyledMapPartnersDirective implements OnChanges {
       fontSize = 14.5;
       x = "5"
     };
-    generatedString = '<text x="' + x + '" y="85" font-family="Helvetica Neue" font-size="' + fontSize + '" fill="#E61B5A">' + bonusString + '</text>'
+    generatedString = '<text x="' + x + '" y="90" font-family="Helvetica Neue" font-size="' + fontSize + '" fill="#E61B5A">' + bonusString + '</text>';
     return generatedString;
   }
 
-  partnerElement(partnerString){}
+  partnerElement(partnerString){
+    let x;
+    let fontSize;
+    let stringLength = partnerString.length;
+    let generatedString;
+    if(stringLength < 11){
+      fontSize = 22;
+    }
+    else if (stringLength < 16){
+      fontSize = 14;
+    }
+    else if (stringLength < 22){
+      fontSize = 12
+    }
+    else{
+      fontSize = 10;
+    };
+    generatedString = '<text x="3" y="40" font-family="Helvetica Neue" font-size="' + fontSize + '">' + partnerString + '</text>';
+    return generatedString;
+  }
 
 
   private placeMarkers(map) {
@@ -91,18 +129,16 @@ export class StyledMapPartnersDirective implements OnChanges {
         this.getImageAsBase64(partner.logoUrlForGMap, (imageAsBase64, validImage) => {
           let latitude = partner.location.latitude;
           let longitude = partner.location.longitude;
+          let imageIcon;
+          let textIcon;
+          if (validImage){imageIcon = 'data:image/svg+xml;utf8,' + svg + '<image x="10" y="-18" width="110" height="110" xlink:href=' +  '"' + imageAsBase64 + '"/>' + this.bonusElement(partner.pfBonus)  + '</svg>';}
+          else {textIcon = 'data:image/svg+xml;utf8,' + svg  + this.bonusElement(partner.pfBonus) + this.partnerElement(partner.nameOrigin) + '</svg>';}
+          let icon = (validImage)? imageIcon : textIcon;
           let marker = new google.maps.Marker({
             position: new google.maps.LatLng(latitude, longitude),
-            map: map;
-            icon
+            map: map,
+            icon: icon
           });
-          if(validImage){
-            marker.icon = 'data:image/svg+xml;utf8,' + svg + "'" + imageAsBase64 + "'" + "/>" + this.bonusElement(partner.pfBonus) + "'" + '</svg>';
-            console.log(marker);
-          }
-          else{
-            // marker.icon = 'data:image/svg+xml;utf8,' + svg + "'" + imageAsBase64 + "'" + "/>" + this.bonusElement(partner.pfBonus) + "'" + '</svg>'
-          };
           markers.push(marker);
           google.maps.event.addListener(marker, 'click', (function (marker) {
             return function () {
