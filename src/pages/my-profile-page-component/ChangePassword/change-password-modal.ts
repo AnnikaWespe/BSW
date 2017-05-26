33e6e3f4ed87a4a44769d308a20997cacc1d65f9
Annika Westphäling <westphaeling@mobile-software.ag>
2017-05-26 17:43:51 +0200

    FA

--- src/pages/my-profile-page-component/ChangePassword/change-password-modal.ts
+++ src/pages/my-profile-page-component/ChangePassword/change-password-modal.ts
@@ -1,22 +1,45 @@
-import {Component, trigger, state, style} from '@angular/core';
+import {Component, trigger, state, style, OnInit} from '@angular/core';
 import {NavController, NavParams, ViewController} from 'ionic-angular';
+import {FormGroup, Validators, FormBuilder} from "@angular/forms";
 
 
 @Component({
   selector: 'change-password-modal',
   templateUrl: 'change-password-modal.html',
 })
-export class ChangePasswordModal {
+export class ChangePasswordModal implements OnInit {
   title: string = "Passwort ändern";
+  private passwordForm: FormGroup;
+  oldPassword: string;
+  newPassword: string;
+
 
 
   constructor(public navCtrl: NavController,
               public navParams: NavParams,
-              public viewCtrl: ViewController) {
+              public viewCtrl: ViewController,
+              private formBuilder: FormBuilder) {
   }
 
   navigateBack() {
     this.viewCtrl.dismiss();
+  }
+
+  ngOnInit(){
+    this.passwordForm = this.formBuilder.group({
+      oldPassword: [this.oldPassword, [
+        Validators.required,
+        Validators.maxLength(24),
+      ]],
+      newPassword: [this.oldPassword, [
+        Validators.required,
+        Validators.maxLength(24),
+      ]],
+      newPasswordRepeat: [this.oldPassword, [
+        Validators.required,
+        Validators.maxLength(24),
+      ]],
+    });
   }
 
 }
