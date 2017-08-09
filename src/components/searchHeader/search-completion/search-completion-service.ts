512bcc31d8f556ff2fce01eae624d9ab28e589a9
Markus Fußenegger <fussenegger@mobile-software.ag>
2017-08-09 13:37:09 +0200

    removed all autosuggestion results which have the url property

--- src/components/searchHeader/search-completion/search-completion-service.ts
+++ src/components/searchHeader/search-completion/search-completion-service.ts
@@ -1,17 +1,17 @@
-
-import { Injectable } from '@angular/core';
-import { Http, Response } from '@angular/http';
-import { Observable } from 'rxjs/Observable';
+import {Injectable} from '@angular/core';
+import {Http, Response} from '@angular/http';
+import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/map';
 
 @Injectable()
 export class SearchCompletionService {
-  constructor (private http: Http) {}
+  constructor(private http: Http) {
+  }
 
-  getSuggestions (searchTerm, latitude = "0", longitude = "0"): Observable<any> {
+  getSuggestions(searchTerm, latitude = "0", longitude = "0"): Observable<any> {
     let rootOfUrl = "https://www.bsw.de/autocomplete/completephrasep?prefix=";
-    let requestUrl : string = rootOfUrl + searchTerm + "&hal&radius=50&latitude=" + latitude + "&longitude=" + longitude + "&callback=www.bsw.de";
+    let requestUrl: string = rootOfUrl + searchTerm + "&hal&radius=50&latitude=" + latitude + "&longitude=" + longitude + "&callback=www.bsw.de";
     console.log(requestUrl);
     return this.http.get(requestUrl)
       .map(this.extractData)
@@ -21,11 +21,26 @@ export class SearchCompletionService {
   private extractData(res: Response) {
     console.log(res);
     let helperString = res.text().replace("www.bsw.de", "");
-    let body = JSON.parse(helperString.substring(1, helperString.length-1));
+    let body = JSON.parse(helperString.substring(1, helperString.length - 1));
     console.log(body);
+
+
+    /* removing all autocompletion results which have the URL property
+     * these values are exclusivly used by the bsw website
+     * */
+    for (var i = body.results.length - 1; i >= 0; i--) {
+
+      if (body.results[i].hasOwnProperty('url')) {
+        body.results.splice(i, 1);
+      }
+
+    }
+
+
     return body.results || [];
   }
-  private handleError (error: Response | any) {
+
+  private handleError(error: Response | any) {
     // TODO: In a real world app, we might use a remote logging infrastructure
     let errMsg: string;
     if (error instanceof Response) {
