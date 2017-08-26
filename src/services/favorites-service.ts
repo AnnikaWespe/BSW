f4e5081a889e05d5b4f109b144928dfe6c78539f
Markus Fußenegger <fussenegger@mobile-software.ag>
2017-08-26 12:56:06 +0200

    load auth before every call - quick fix

--- src/services/favorites-service.ts
+++ src/services/favorites-service.ts
@@ -12,37 +12,59 @@ export class FavoritesService {
 
   constructor(private http: Http, private envService: EnvironmentService) {
     this.favoritesUrlSnippet = this.envService.environment.BASE_URL + this.envService.environment.GET_FAVORITES;
-    this.securityToken = encodeURIComponent(localStorage.getItem("securityToken"));
-    this.mitgliedId = localStorage.getItem("mitgliedId");
+    this.loadAuthData();
   }
 
-  createAuthorizationHeader(headers: Headers) {
+  private static createAuthorizationHeader(headers: Headers) {
     headers.append('Authorization', 'Basic ' +
       btoa('BSW_App:ev1boio32fSrjSY9XwvcD9LkGr13J'));
   }
 
-  get(url) {
+  private get(url) {
     let headers = new Headers({ 'Accept': 'application/json' });
-    this.createAuthorizationHeader(headers);
+    FavoritesService.createAuthorizationHeader(headers);
     return this.http.get(url, {
       headers: headers
     });
   }
 
   getFavorites(id, token){
-    token = encodeURIComponent(token);
-    let url = this.favoritesUrlSnippet + 'get?mandant_id=1&mitglied_id=' + id + '&securityToken=' + token;
-    console.log("getting favorites with url", url);
+
+    this.loadAuthData();
+    let url = this.favoritesUrlSnippet + 'get?mandant_id=1&mitglied_id=' + id + '&securityToken=' + encodeURIComponent(token);
     return(this.get(url));
+
   }
+
   deleteFavorite(pfNumber){
+
+    this.loadAuthData();
     let url = this.favoritesUrlSnippet + 'loeschen.json?mandant_id=1&mitglied_id=' + this.mitgliedId + '&pfNummer=' + pfNumber +  '&securityToken=' + this.securityToken;
     return(this.get(url));
+
   }
+
   rememberFavorite(pfNumber){
+
+    this.loadAuthData();
     let url = this.favoritesUrlSnippet + 'merken.json?mandant_id=1&mitglied_id=' + this.mitgliedId + '&pfNummer=' + pfNumber + '&securityToken=' + this.securityToken;
     return(this.get(url));
+
   }
+
+  /*
+   *
+   * function which allows to load auth data before every call
+   * auth data should be managed in a service
+   *
+   *  */
+  private loadAuthData(){
+
+    this.securityToken = encodeURIComponent(localStorage.getItem("securityToken"));
+    this.mitgliedId = localStorage.getItem("mitgliedId");
+
+  }
+
 }
 
 
