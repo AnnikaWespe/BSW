26966bd103bf019a661c4833ade19d3880a31bc4
Markus Fußenegger <fussenegger@mobile-software.ag>
2017-08-30 18:23:42 +0200

    increased webview timeout to 15 seconds

--- src/pages/webview/webview.ts
+++ src/pages/webview/webview.ts
@@ -155,7 +155,7 @@ export class WebviewComponent implements OnDestroy, AfterViewInit {
 
     if (this.iframe) {
 
-      this.timeoutHandle = setTimeout(() => {this.errorLoad()}, 5000);
+      this.timeoutHandle = setTimeout(() => {this.errorLoad()}, 15000);
       this.iframe.nativeElement.onload = () => {
         clearTimeout(this.timeoutHandle);
         this.dismissLoadingIndicator();
